eed941d9bcc7935238a7c0079dc35a211ab5cb5d
SerDanPy <serdaniil88@gmail.com>
2025-09-09 18:02:07 +0100

    Update circle.py

--- circle.py
+++ circle.py
@@ -5,11 +5,13 @@ Use this function in a program that asks the user for a radius and prints out th
 
 import math
 
-#function that calculates the radius of a circle
+
+# Calculate the area of a circle given its radius
 def calculate_circle_area(radius):
     area = math.pi * radius ** 2
     return area
 
 #request to user and output of the result
 radius = float(input("Enter the radius of a circle: "))
+
 print("Circle area: ", round(calculate_circle_area(radius),2))
